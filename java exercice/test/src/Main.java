0526ea0d7dd8bc51c88d8b48775bd81be14a0a59
aya <ayalee1209@gmail.com>
2024-03-06 08:06:32 +0100

    first frame

---
+++ java exercice/test/src/Main.java
@@ -0,0 +1,17 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        /*JFrame frame = new JFrame();
+		frame.setVisible(true);
+		frame.setSize(600,420);
+		frame.setTitle("my first frame");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        ImageIcon image = new ImageIcon("src/Les 100 plus belles photos non photoshopées.jpg");
+        frame.setIconImage(image.getImage());
+        frame.getContentPane().setBackground(new Color(45,200,134));*/
+         MyFrame myframe = new MyFrame();
+    }
+    
+}
